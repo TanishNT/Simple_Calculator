e40e39822ef31140a1bdf93502739da9effd2e80
Tanish <vpc75500@gmail.com>
2023-07-13 18:17:37 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let outputScreen = document.getElementById("output-screen");
+function display(num) {
+  outputScreen.value += num;
+}
+function calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (e) {
+    alert("Invalid Input");
+  }
+}
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
